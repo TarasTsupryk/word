bf04783fc178dba3528c60960058a3153761ef8f
Taras Tsupryk <taras.tsupryk@intellias.com>
2023-04-16 00:23:24 +0300

    Add game page

---
+++ src/pages/Game.page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Game } from 'modules/Game';
+
+const GamePage = () => {
+    return <Game />
+}
+
+export default GamePage;
+
+
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,10 +1,15 @@
 import { RouteProps } from "react-router";
 import { ROUTES } from "common";
 import HomePage from "./Home.page";
+import GamePage from "./Game.page";
 
 export const PAGES: RouteProps[] = [
   {
     path: ROUTES.HOME,
     Component: HomePage,
+  },
+  {
+    path: ROUTES.GAME,
+    Component: GamePage,
   },
 ];
