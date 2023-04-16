1ca20d629fafa289088e80173954a2bd32813308
Taras Tsupryk <taras.tsupryk@intellias.com>
2023-04-16 19:46:51 +0300

    add allWords constant

---
+++ src/common/allWords.ts
@@ -0,0 +1,67 @@
+import a from 'assets/dataset/а.json'; 
+import b from 'assets/dataset/б.json'; 
+import v from 'assets/dataset/в.json'; 
+import h from 'assets/dataset/г.json'; 
+import d from 'assets/dataset/д.json'; 
+import e from 'assets/dataset/е.json'; 
+import zh from 'assets/dataset/ж.json'; 
+import z from 'assets/dataset/з.json'; 
+import y from 'assets/dataset/й.json'; 
+import k from 'assets/dataset/к.json'; 
+import l from 'assets/dataset/л.json'; 
+import m from 'assets/dataset/м.json'; 
+import n from 'assets/dataset/н.json'; 
+import o from 'assets/dataset/о.json'; 
+import p from 'assets/dataset/п.json'; 
+import r from 'assets/dataset/р.json'; 
+import s from 'assets/dataset/с.json'; 
+import t from 'assets/dataset/т.json'; 
+import u from 'assets/dataset/у.json'; 
+import f from 'assets/dataset/ф.json'; 
+import kh from 'assets/dataset/х.json'; 
+import ts from 'assets/dataset/ц.json'; 
+import ch from 'assets/dataset/ч.json'; 
+import sh from 'assets/dataset/ш.json'; 
+import shch from 'assets/dataset/щ.json'; 
+import yu from 'assets/dataset/ю.json'; 
+import ya from 'assets/dataset/я.json'; 
+import ye from 'assets/dataset/є.json'; 
+import i from 'assets/dataset/і.json'; 
+import yi from 'assets/dataset/ї.json'; 
+import g from 'assets/dataset/ґ.json'; 
+
+const allWords = {
+    "А": a.array,
+    "Б": b.array,
+    "В": v.array,
+    "Г": h.array,
+    "Ґ": g.array,
+    "Д": d.array,
+    "Е": e.array,
+    "Є": ye.array,
+    "Ж": zh.array,
+    "З": z.array,
+    "І": i.array,
+    "Ї": yi.array,
+    "Й": y.array,
+    "К": k.array,
+    "Л": l.array,
+    "М": m.array,
+    "Н": n.array,
+    "О": o.array,
+    "П": p.array,
+    "Р": r.array,
+    "С": s.array,
+    "Т": t.array,
+    "У": u.array,
+    "Ф": f.array,
+    "Х": kh.array,
+    "Ц": ts.array,
+    "Ч": ch.array,
+    "Ш": sh.array,
+    "Щ": shch.array,
+    "Ю": yu.array,
+    "Я": ya.array,
+}
+
+export default allWords;
