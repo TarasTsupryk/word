cee04220302825787739c8454d6f889de3b4d507
Taras Tsupryk <taras.tsupryk@intellias.com>
2023-04-17 00:15:19 +0300

    Add theme

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  blue: "#2972FE",
+  lightBlue: "#548efe",
+  red: "",
+  lightRed: "#F17F78",
+  pink: "#ef5da8",
+  lightPink: "",
+};
---
+++ src/styles/spacings.ts
@@ -0,0 +1,12 @@
+export const spacings = {
+  hasNoSpace: "0px",
+  spacing2: "2px",
+  spacing4: "4px",
+  spacing8: "8px",
+  spacing12: "12px",
+  spacing16: "16px",
+  spacing20: "20px",
+  spacing24: "24px",
+  spacing28: "28px",
+  spacing32: "32px",
+};
